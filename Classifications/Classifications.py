c588b92c44da693ed49fe1ddb62c1df46c788b1f
Nobody <mishasikolenko@mail.ru>
2023-07-19 11:48:56 +0300

    Bais classification

--- Classifications/Classifications.py
+++ Classifications/Classifications.py
@@ -2,21 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 N = 5
+bias = 3
 
 def __main__():
 	x1 = np.random.random(N)
-	x2 = x1 + [np.random.randint(10) / 10 for i in range(N)]
+	x2 = x1 + [np.random.randint(10) / 10 for i in range(N)] + bias
 	C1 = [x1, x2]
 
 	x1 = x1 = np.random.random(N)
-	x2 = x1 - [np.random.randint(10) / 10 for i in range(N)] - 0.1
+	x2 = x1 - [np.random.randint(10) / 10 for i in range(N)] - 0.1 + bias
 	C2 = [x1, x2]
 
-	f = [0, 1]
+	f = [0 + bias, 1 + bias]
 
-	w = np.array([-0.3, 0.3])
+	w2 = 0.5
+	w3 = -bias + w2
+	w = np.array([-w2, w2, w3])
 	for i in range(N):
-		x = np.array([C1[0][i], C1[1][i]])
+		x = np.array([C1[0][i], C1[1][i], 1])
 		y = np.dot(w, x)
 		if y >= 0:
 			print("C1")
