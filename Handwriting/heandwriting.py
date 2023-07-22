75eedaec70bd2ebc1f53fd393fbbe20700023810
Nobody <mishasikolenko@mail.ru>
2023-07-22 23:02:31 +0300

    Make up the model

--- Handwriting/heandwriting.py
+++ Handwriting/heandwriting.py
@@ -9,6 +9,8 @@ from tensorflow import keras
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.layers import Dense, Flatten
 
+WHITE_MAX = 255
+
 def show(x_train, N):
 	plt.figure(figsize = (10, 5))
 	for i in range(N):
@@ -21,13 +23,25 @@ def show(x_train, N):
 
 
 def __main__():
+
 	(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-	x_train = x_train / 255
-	y_train = y_train / 255
+	# Normalized
+	x_train = x_train / WHITE_MAX
+	y_train = y_train / WHITE_MAX
 
-	show(x_train, 25)
+	#show(x_train, 25)
 
+	# Make vectors instad of numbers 
+	y_train_cat = keras.utils.to_categorical(y_train, 10)
+	y_test_cat = keras.utils.to_categorical(y_test, 10)
+
+	model = keras.Sequential([Flatten(input_shape = (28, 28, 1)), 
+		Dense(128, activation = 'relu'), Dense(10, activation = 'softmax')])
+
+	print(model.summary())
+
+	
 
 if __name__ == '__main__':
 	__main__()
