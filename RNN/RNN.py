d48bf9af7e16b3fc94810456f5fe40c91132984f
Nobody <mishasikolenko@mail.ru>
2023-08-10 14:31:47 +0300

    Make char's predicted

---
+++ RNN/RNN.py
@@ -0,0 +1,83 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import glob
+import re
+
+from PIL import Image
+from random import randint
+from tensorflow import keras
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Dense, SimpleRNN, Input
+from tensorflow.keras.preprocessing.text import Tokenizer
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from skimage.color import rgb2lab, lab2rgb
+from skimage.io import imsave
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+num_characters = 40
+
+def buildPhrase(inp_str, inp_chars, model, tokenizer, str_len = 200):
+
+	global num_characters
+
+	for i in range(str_len):
+		x = []
+		for j in range(i, i + inp_chars):
+			x.append(tokenizer.texts_to_matrix([inp_str[j]]))
+
+		x = np.array(x)
+		inp = x.reshape(1, inp_chars, num_characters)
+
+		pred = model.predict(inp)
+		d = tokenizer.index_word[pred.argmax(axis = 1)[0]]
+
+		inp_str += d
+
+	return inp_str
+
+
+def __main__():
+
+	global num_characters
+
+	with open('train_data_true', 'r', encoding = 'utf-8') as f:
+		text = f.read()
+		test = text.replace('\ufeff', ' ')
+		text = re.sub(r'[^А-я –.,?!:]', '', text)
+
+	tokenizer = Tokenizer(num_words = num_characters, char_level = True)
+	tokenizer.fit_on_texts([text])
+	print(tokenizer.word_index)
+
+	input_str = 'Кармон:'
+	inp_chars = len(input_str)
+	data = tokenizer.texts_to_matrix(text)
+	n = data.shape[0] - inp_chars
+
+	X = np.array([data[i:i + inp_chars, :] for i in range(n)])
+	Y = data[inp_chars:]
+
+	model = Sequential()
+	model.add(Input((inp_chars, num_characters)))
+	model.add(SimpleRNN(128, activation = 'tanh'))
+	model.add(Dense(num_characters, activation = 'softmax'))
+	model.summary()
+
+	model.compile(loss = 'categorical_crossentropy', metrics = ['accuracy'], optimizer = 'adam')
+
+	history = model.fit(X, Y, batch_size = 64, epochs = 150)
+
+	res = buildPhrase(input_str, inp_chars, model, tokenizer)
+	print(res)
+
+
+if __name__ == '__main__':
+	__main__()
+
